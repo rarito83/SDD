88c5846fb6fc9b94572bcb98fb574c3b1515d9e2
Rahmat Susanto <rarito.jogja@gmail.com>
2023-09-30 12:24:36 +0700

    Initial Commit

---
+++ SDD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SDD
+//
+//  Created by Rahmat Susanto on 30/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
